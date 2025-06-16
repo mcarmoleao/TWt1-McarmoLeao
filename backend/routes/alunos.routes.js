82992cb038cb15d5047c129bd46b23f1c2a67618
Maria do Carmo Leão <maria.leao@ipvc.pt>
2025-06-16 13:13:58 +0100

    edit aluno

--- backend/routes/alunos.routes.js
+++ backend/routes/alunos.routes.js
@@ -5,5 +5,6 @@ const alunoController = require('../controllers/aluno.controller');
 router.get('/', alunoController.getAlunos);
 router.post('/', alunoController.addAluno);
 router.delete('/:id', alunoController.deleteAluno);
+router.put('/:id', alunoController.editAluno);
 
 module.exports = router;
